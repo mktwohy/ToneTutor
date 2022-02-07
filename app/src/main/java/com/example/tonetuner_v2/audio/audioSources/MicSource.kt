dca53db9f2c6b31915a66ae257426af04be403c6
mktwohy <mktwohy@gmail.com>
2022-02-06 21:49:40 -0600

    MicSource no longer implements Runnable, as it was unnecessary  - MicSource is not supposed to run on another thread  - Kotlin allows Threads with lambdas  - doing this removes the need for two fields (recorder and recordThread)  - this is especially helpful because these parameters were initialized as null, so this change eliminates a ton of null checks.

--- app/src/main/java/com/example/tonetuner_v2/audio/audioSources/MicSource.kt
+++ app/src/main/java/com/example/tonetuner_v2/audio/audioSources/MicSource.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.media.AudioFormat
 import android.media.AudioRecord
 import android.media.MediaRecorder
-import android.util.Log
 import com.example.tonetuner_v2.app.AppModel.CAPTURE_BUFFER_SIZE
 import com.example.tonetuner_v2.app.AppModel.SAMPLE_RATE
 import com.example.tonetuner_v2.checkMicPermission
@@ -19,47 +18,12 @@ class MicSource(
     val context: Context,
     val sampleRate: Int = SAMPLE_RATE,
     val bufferSize: Int = CAPTURE_BUFFER_SIZE,
-) : Runnable, AudioSource {
-    private var recorder: AudioRecord? = null
-    private var recordThread: Thread? = null
+) : AudioSource {
     private var running = false
     private val queue: BlockingQueue<Float> = ArrayBlockingQueue(bufferSize)
 
     init {
-        /** Open the microphone and create the recording thread.*/
-
-        // todo: Figure out how to properly get permissions from user
-            // This works now, but I'm not sure why this is still red underlined
-
         startCapture()
-    }
-
-    /** Begin capturing and buffering audio */
-    fun startCapture() {
-        if (running) return
-
-        if (!checkMicPermission(context)){
-            running = false
-            return
-        }
-
-        if (recorder == null){
-            recorder = AudioRecord(
-                MediaRecorder.AudioSource.DEFAULT,
-                sampleRate,
-                AudioFormat.CHANNEL_IN_MONO,
-                AudioFormat.ENCODING_PCM_FLOAT,
-                bufferSize
-            )
-        }
-
-
-        if (recordThread == null ) {
-            recordThread = Thread(this)
-            recordThread?.start()
-        }
-
-        running = true
     }
 
     /**
@@ -71,36 +35,43 @@ class MicSource(
         List<Float>(bufferSize) { queue.take() }
 
 
-    /**
-     * Audio capture thread entry point.
-     *
-     * Captures and buffers audio data.
-     */
-    override fun run() {
-        android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO)
+    /** Begin capturing and buffering audio */
+    fun startCapture() {
+        if (running) return
 
-        if (recorder?.state != AudioRecord.STATE_INITIALIZED) {
-            Log.e("AudioProc", "Audio Record can't initialize!")
+        if (!checkMicPermission(context)){
+            running = false
             return
         }
-        recorder?.startRecording()
 
-        Log.v("AudioProc", "Start recording")
+        Thread {
+            android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_AUDIO)
 
-        val audioBuffer = FloatArray(bufferSize)
-        while (running) {
+            val buffer = FloatArray(bufferSize)
+            val ar = AudioRecord(
+                MediaRecorder.AudioSource.DEFAULT,
+                sampleRate,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_FLOAT,
+                bufferSize
+            )
 
-            // Fetch data from the microphone
-            val numread = recorder?.read(audioBuffer, 0,
-                audioBuffer.size, AudioRecord.READ_BLOCKING)
+            ar.startRecording()
+            running = true
+            while (running) {
+                // Fetch data from the microphone
+                ar.read(buffer, 0,
+                    buffer.size, AudioRecord.READ_BLOCKING)
 
-            // Put data into the blocking queue
-            audioBuffer.forEach { queue.offer(it) }
-
-            // Todo if something broke, uncomment this:
-            // recordThread?.isAlive
-        }
-        recorder?.stop()
-        recorder?.release()
+                // Put data into the blocking queue
+                buffer.forEach { queue.offer(it) }
+            }
+            ar.stop()
+            ar.release()
+        }.start()
     }
+
+    fun stopCapture(){ running = false }
+
+
 }
