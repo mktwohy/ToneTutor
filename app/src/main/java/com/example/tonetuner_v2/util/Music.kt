1feb7c08a3d8c5b53e607249b43c4ce0a53fb888
mktwohy <69822801+mktwohy@users.noreply.github.com>
2023-02-24 23:15:54 -0600

    refactor Music.kt

--- app/src/main/java/com/example/tonetuner_v2/util/Music.kt
+++ app/src/main/java/com/example/tonetuner_v2/util/Music.kt
@@ -7,6 +7,7 @@ import com.example.signallib.enums.Note.Companion.plus
 import kotlin.math.abs
 import kotlin.math.absoluteValue
 import kotlin.math.log
+import kotlin.math.sign
 
 fun Note.calcInterval(other: Note): Interval {
     if (this == other) return Interval.PER_1
@@ -17,50 +18,49 @@ fun Note.calcInterval(other: Note): Interval {
 }
 
 fun calcFreq(note: Note, cents: Int): Float {
-    val sign = if (cents > 0) 1 else -1
-    val noteNeighbor = note + sign
-    val centsAsHz = sign * ((note.freq - noteNeighbor.freq) * cents / 100).absoluteValue
+    val noteNeighbor = if (cents > 0) note + 1 else note - 1
+    val centsAsHz = cents.sign * ((note.freq - noteNeighbor.freq) * cents / 100).absoluteValue
     return note.freq + centsAsHz
 }
 
 /** from https://psychology.wikia.org/wiki/Pitch_perception */
-fun freqToPitch(freq: Float) = 69 + 12 * log(freq / 440f, 2f)
+fun freqToPitch(freq: Float): Float =
+    69 + 12 * log(freq / 440f, 2f)
 
-// todo this shouldn't be an extension function
-/** Converts frequency to closest note estimate*/
-fun Float.toNote(): Note? {
+fun freqToClosestNote(frequency: Float): Note? {
     // check if frequency is out of bounds
-    if (this < Note.C_0.freq || this > Note.B_8.freq) return null
+    if (frequency < Note.C_0.freq || frequency > Note.B_8.freq) return null
 
     // find the upper estimate for the note
     var upperEst = Note.Cs0
-    while (upperEst.freq < this && upperEst != Note.B_8) {
+    while (upperEst.freq < frequency && upperEst != Note.B_8) {
         upperEst += 1
     }
 
     // get the lower estimate for the note
     val lowerEst = upperEst - 1
 
-    val upperErr = abs(upperEst.freq - this)
-    val lowerErr = abs(lowerEst.freq - this)
+    val upperErr = abs(upperEst.freq - frequency)
+    val lowerErr = abs(lowerEst.freq - frequency)
 
     return if (upperErr < lowerErr) upperEst else lowerEst
 }
 
-/** Converts frequency to the closest note and its error (cents) */
-fun Float.toNoteAndCents(): Pair<Note?, Int> {
-    val note = this.toNote() ?: return Pair(null, 0)
-    val hzError = this - note.freq
-
-    val centsError =
-        if (hzError > 0) {
+fun freqToNoteAndCents(frequency: Float): Pair<Note?, Int> {
+    val note = freqToClosestNote(frequency) ?: return Pair(null, 0)
+    val hzError = frequency - note.freq
+    val centsError = when {
+        hzError > 0 -> {
             val hzToNextNote = (note + 1).freq - note.freq
             (100 * hzError / hzToNextNote).toInt()
-        } else {
-            if (note == Note.C_0) { 0 } else {
-                val hzToPrevNote = note.freq - (note - 1).freq
-                (100 * hzError / hzToPrevNote).toInt()
-            }
         }
+        note == Note.C_0 -> {
+            0
+        }
+        else -> {
+            val hzToPrevNote = note.freq - (note - 1).freq
+            (100 * hzError / hzToPrevNote).toInt()
+        }
+    }
     return Pair(note, centsError)
 }
